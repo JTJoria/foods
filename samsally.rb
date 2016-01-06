e21117e931c69410ee102338916da8e6a73158f5
TIM JORIA <jtjoria@gmail.com>
2016-01-05 19:18:04 -0600

    ruby_exercises

---
+++ samsally.rb
@@ -0,0 +1,38 @@
+puts "How many recipes can Sam make?"
+  recipes = gets.chomp.to_i
+
+puts "How many languages can Sally speak?"
+  languages = gets.chomp.to_i
+
+puts "Can Sam make crepes? Please enter yes or no."
+  yes = gets.chomp
+  yes = true
+
+puts "Can Sally speak French? Please enter yes1 or no1."
+  yes1 = gets.chomp
+  yes1 = true
+
+if recipes > 10 and languages >5
+  puts "Sam and Sally should date."
+end
+
+if yes || yes1 = true
+  put=begin
+end
+s "Sam and Sally should get married."
+end 
+  
+
+  
+
+
+
+
+
+=begin
+If Sam can cook more than 10 recipes and 
+Sally speaks more than 5 languages, 
+they should date. 
+If Sam can make crepes or 
+Sally can speak French, 
+they should marry. =end
