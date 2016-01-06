745a8d87a1194ffad9cb1ba47a1528acbe2ec4ce
TIM JORIA <jtjoria@gmail.com>
2016-01-05 21:21:42 -0600

    classroom

--- bankcorrect.rb
+++ bankcorrect.rb
@@ -33,5 +33,31 @@ accounts = []
              }
 end
 
+puts ""
+puts "Thank you, here is your report."
+puts ""
 
-p accounts
+puts " Report ".center(50, " ")
+puts Array.new(50){"*"}.join
+
+accounts.each do |account|
+
+  puts "FIRST NAME #{ account [:first_name] }"
+  puts ""
+
+  puts "LAST NAME #{account [:last_name] }"
+  puts ""
+
+  puts "EMAIL: #{account [:email] }"
+  puts ""
+
+  puts "ACCT # : #{account [:acct_num]}"
+
+  puts ""
+  puts "*******************************"
+  puts ""
+
+end
+
+puts ""
+puts Aray.new(50){"*"}.join
---
+++ classroom.rb
@@ -0,0 +1,14 @@
+# enter an even number ofnames
+# when all names enter type done
+# randomize names
+# puts names out in groups of two
+
+
+puts "Please enter the students' names and press enter"
+puts "When all students have been inputed, type done"
+
+names = []
+
+names = gets.chomp
+
+until 
